e6572976827d46ccd6a033d65f6068d906e1b9aa
Ben Marini <bmarini@gmail.com>
2013-08-07 08:30:08 -0700

    First try at bob for elixir

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,24 @@
+defmodule Teenager do
+  defmodule Statement do
+    def nothing?(statement) do
+      statement == nil || String.strip(statement) == ""
+    end
+
+    def yelling?(statement) do
+      String.upcase(statement) == statement
+    end
+
+    def question?(statement) do
+      String.at(statement, -1) == "?"
+    end
+  end
+
+  def hey(statement) do
+    cond do
+      Statement.nothing?(statement) -> "Fine. Be that way!"
+      Statement.yelling?(statement) -> "Woah, chill out!"
+      Statement.question?(statement) -> "Sure."
+      true -> "Whatever."
+    end
+  end
+end

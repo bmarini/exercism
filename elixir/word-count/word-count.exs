2ea611a50b3b3f1ee581695181d2a01f3724df0d
Ben Marini <bmarini@gmail.com>
2013-08-08 08:38:50 -0700

    Change sentence to phrase, tighter regexp

--- elixir/word-count/word-count.exs
+++ elixir/word-count/word-count.exs
@@ -2,20 +2,20 @@ defmodule Words do
   import String, only: [ downcase: 1, split: 2 ]
   import Regex, only: [ replace: 3 ]
 
-  def count(sentence) do
-    sentence
+  def count(phrase) do
+    phrase
     |> downcase
     |> remove_punctuation
     |> to_word_list
     |> _count( HashDict.new )
   end
 
-  def remove_punctuation(sentence) do
-    replace( %r/[^a-z0-9A-Z ]/, sentence, "" )
+  defp remove_punctuation(phrase) do
+    replace( %r/[^a-z0-9\s]/, phrase, "" )
   end
 
-  def to_word_list(sentence) do
-    split( sentence, %r/\s+/ )
+  defp to_word_list(phrase) do
+    split( phrase, %r/\s+/ )
   end
 
   defp _count( [ word | rest ], hash ) do

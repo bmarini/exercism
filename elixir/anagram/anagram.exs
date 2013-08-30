b9bc320f82ef132a1d86b8b7448a85e97b93860f
Ben Marini <bmarini@gmail.com>
2013-08-29 21:58:38 -0700

    Better var names

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -1,14 +1,14 @@
 defmodule Anagram do
-  import String, only: [ downcase: 1, codepoints: 1]
-  import Enum, only: [ filter: 2, sort: 1]
+  import String, only: [ downcase: 1, codepoints: 1 ]
+  import Enum, only: [ filter: 2, sort: 1 ]
 
-  def match(word, words) do
-    filter words, anagram?(&1, word)
+  def match(subject, candidates) do
+    filter candidates, anagram?(subject, &1)
   end
 
-  def anagram?(a, a), do: false
-  def anagram?(a, b) do
-    sorted_codepoints(a) == sorted_codepoints(b)
+  def anagram?(_subject, _subject), do: false
+  def anagram?(subject, candidate) do
+    sorted_codepoints(subject) == sorted_codepoints(candidate)
   end
 
   defp sorted_codepoints(string) do
